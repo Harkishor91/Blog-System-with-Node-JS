acb5d1fa9701c37e79682994f8bd1b5923227483
Harkishor <harkishor5622@gmail.com>
2024-08-03 19:51:36 +0530

    Added ForgotPassword and ResetPassword API

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const authMiddleware = require("../middlewares/authentication");
-const {validateRegisterUser} = require("../middlewares/validations");
+const { validateRegisterUser } = require("../middlewares/validations");
+const imageUpload = require("../middlewares/Imageupload");
 
 const router = express.Router();
 
@@ -9,12 +10,21 @@ const {
   registerUser,
   loginUser,
   getAllUsers,
+  forgotPassword,
+  resetPassword
 } = require("./../controllers/userController");
 
 // define all routes
-router.post("/registerUser", validateRegisterUser, registerUser);
+router.post(
+  "/registerUser",
+  validateRegisterUser,
+  // imageUpload.single("profileImage"),
+  registerUser
+);
 router.post("/loginUser", loginUser);
 // use authentication for get user list
 router.get("/getAllUsers", authMiddleware, getAllUsers);
+router.post("/forgotPassword", authMiddleware, forgotPassword);
+router.post("/resetPassword", authMiddleware, resetPassword);
 
 module.exports = router;
